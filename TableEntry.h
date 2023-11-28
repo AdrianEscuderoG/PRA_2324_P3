56d5317c14a1b75bc77798474387bd8fe3c59c27
AESCGAR3 <aescgar3@alumno.upv.es@ldsic-vdi10.upvnet.upv.es>
2023-11-28 19:53:52 +0100

    Clase TableEntry

---
+++ TableEntry.h
@@ -0,0 +1,31 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <iostream>
+
+template <typename V> 
+class TableEntry {
+    public:
+  	std::string key;
+	V value;
+	TableEntry(std::string key, V value){this->key=key;this->value=value;}
+	TableEntry(std::string key){this->key=key;value=NULL;}
+	TableEntry(){key="";value=NULL;}
+	friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		if(te1.key==te2.key)
+			return true;
+		return false;
+	}
+	friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+                if(te1.key==te2.key)
+                        return false;
+                return true;
+        }
+	friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+		out<<te.key<<"->"<<te.value<<"\n";
+		return out;
+	}
+};
+
+#endif
